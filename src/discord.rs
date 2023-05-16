3360d0e6fe6e3740239698aae3f7ead1f65f73ac
augustine <augustino0890@gmail.com>
2023-05-16 18:11:42 +0900

    create discord client and connect to discord server

---
+++ src/discord.rs
@@ -0,0 +1,32 @@
+use serenity::{
+    async_trait,
+    model::gateway::{GatewayIntents, Ready},
+    prelude::*,
+};
+
+use crate::database::Database;
+
+pub struct Handler {
+    pub db: Database,
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+pub async fn run_discord_bot(token: &str, db: Database) -> tokio::task::JoinHandle<()> {
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler { db })
+        .await
+        .expect("Error creating Discord client");
+
+    let handler = tokio::spawn(async move {
+        client.start().await.expect("Error starting Discord client");
+    });
+
+    handler
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use config::Config;
 use database::Database;
+use discord::run_discord_bot;
 
 mod config;
 mod database;
+mod discord;
 
 #[tokio::main]
 async fn main() {
     let config = Config::new("config.yaml").expect("Failed to read configuration file");
-    let _ = Database::new(&config.mongo_uri)
+    let db = Database::new(&config.mongo_uri)
         .await
         .expect("Failed to connect to database");
-    println!("Connected to database")
+    println!("Connected to database");
+
+    // Run the Discord bot
+    let discord_bot_handle = run_discord_bot(&config.discord_token, db).await;
+    if let Err(why) = discord_bot_handle.await {
+        println!("An error occurred while connecting to Discord: {}", why);
+    }
 }

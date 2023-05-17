e9daa05e824f101a24ab59e54012710e9d87865a
augustine <augustino0890@gmail.com>
2023-05-17 16:44:49 +0900

    add interaction

--- src/discord.rs
+++ src/discord.rs
@@ -1,11 +1,18 @@
+use ethaddr::Address;
+use serenity::model::application::command::Command;
+use serenity::model::prelude::interaction::MessageFlags;
 use serenity::{
     async_trait,
+    model::application::interaction::{Interaction, InteractionResponseType},
     model::gateway::{GatewayIntents, Ready},
-    model::id::GuildId,
+    model::{
+        application::interaction::application_command::ApplicationCommandInteraction, id::GuildId,
+    },
     prelude::*,
 };
+
 use std::env;
-use tracing::info;
+use tracing::{error, info};
 
 use crate::commands;
 use crate::database::Database;
@@ -16,9 +23,24 @@ pub struct Handler {
 
 #[async_trait]
 impl EventHandler for Handler {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        match interaction {
+            Interaction::ApplicationCommand(command) => match command.data.name.as_str() {
+                "exchange" => {
+                    if let Err(why) = self.handle_exchange(ctx.clone(), command).await {
+                        error!("Error handling exchange: {:?}", why);
+                    }
+                }
+                _ => info!("Command not found"),
+            },
+            _ => (),
+        }
+    }
+
     async fn ready(&self, ctx: Context, ready: Ready) {
         info!("{} is connected!", ready.user.name);
 
+        // Get the guild_id from the environment variables
         let guild_id = GuildId(
             env::var("DISCORD_GUILD")
                 .expect("Expected GUILD_ID in environment")
@@ -26,11 +48,121 @@ impl EventHandler for Handler {
                 .expect("GUILD_ID must be an integer"),
         );
 
-        let _ = guild_id
-            .set_application_commands(&ctx.http, |commands| {
-                commands.create_application_command(|command| commands::exchange(command))
-            })
-            .await;
+        // Fetch existing application commands.
+        let existing_commands = guild_id
+            .get_application_commands(&ctx.http)
+            .await
+            .expect("Could not fetch commands");
+
+        // Loop over the existing commands and delete the command named "exchange" if it exists.
+        for command in existing_commands {
+            if command.name == "exchange" {
+                guild_id
+                    .delete_application_command(&ctx.http, command.id)
+                    .await
+                    .expect("Failed to delete command");
+            }
+        }
+
+        // Fetch existing global commands.
+        let global_commands = Command::get_global_application_commands(&ctx.http)
+            .await
+            .unwrap();
+        // Loop over the global commands and delete the command named "exchange" if it exists.
+        for command in global_commands {
+            if command.name == "exchange" {
+                Command::delete_global_application_command(&ctx.http, command.id)
+                    .await
+                    .expect("Failed to delete global command");
+            }
+        }
+
+        // Create the "exchange" command.
+        let new_command = guild_id
+            .create_application_command(&ctx.http, |command| commands::exchange(command))
+            .await
+            .expect("Failed to create command");
+    }
+}
+
+impl Handler {
+    async fn handle_exchange(
+        &self,
+        ctx: Context,
+        command: ApplicationCommandInteraction,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        // Check for the correct channel
+        let attendance_channel: u64 = env::var("ATTENDANCE_CHANNEL")?
+            .parse()
+            .expect("ATTENDANCE_CHANNEL must be an integer");
+        if command.channel_id.as_u64() != &attendance_channel {
+            return Ok(());
+        }
+
+        // Get the options from the command
+        let wallet_address_option = command
+            .data
+            .options
+            .get(0)
+            .and_then(|o| o.value.as_ref())
+            .and_then(|v| v.as_str());
+        let number_of_tickets_option = command
+            .data
+            .options
+            .get(1)
+            .and_then(|o| o.value.as_ref())
+            .and_then(|v| v.as_i64());
+
+        // Check if the wallet address is valid and convert it to checksum format
+        let _ = match wallet_address_option {
+            // let wallet_address = match wallet_address_option {
+            Some(addr) => match Address::from_str_checksum(addr) {
+                Ok(address) => Some(address),
+                Err(_) => {
+                    let _ = command
+                        .create_interaction_response(&ctx.http, |r| {
+                            r.kind(InteractionResponseType::ChannelMessageWithSource)
+                                .interaction_response_data(|m| {
+                                    m.content("Invalid wallet address! Please try again.")
+                                        .flags(MessageFlags::EPHEMERAL)
+                                })
+                        })
+                        .await;
+                    None
+                }
+            },
+            None => {
+                let _ = command
+                    .create_interaction_response(&ctx.http, |r| {
+                        r.kind(InteractionResponseType::ChannelMessageWithSource)
+                            .interaction_response_data(|m| {
+                                m.content("No wallet address provided! Please try again.")
+                                    .flags(MessageFlags::EPHEMERAL)
+                            })
+                    })
+                    .await;
+                None
+            }
+        };
+
+        // Check if the number of tickets is valid
+        let _ = match number_of_tickets_option {
+            // let number_of_tickets = match number_of_tickets_option {
+            Some(num) => num as u8,
+            None => {
+                command
+                    .create_interaction_response(&ctx.http, |r| {
+                        r.kind(InteractionResponseType::ChannelMessageWithSource)
+                            .interaction_response_data(|m| {
+                                m.content("No number of tickets provided! Please try again.")
+                            })
+                    })
+                    .await?;
+
+                return Ok(());
+            }
+        };
+        Ok(())
     }
 }
 

44f0ecfc2583362b5f9668e7410907ea0fb1469a
augustine <augustino0890@gmail.com>
2023-05-22 12:20:55 +0900

    scheduler: run separate asynchronous update status task

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -5,27 +5,57 @@ use std::str::FromStr;
 use tracing::{error, info};
 
 pub async fn setup_scheduler(database: MongoDB) {
-    let schedule = Schedule::from_str("* * * * * 5").unwrap();
-    // let schedule = Schedule::from_str("0 * * * * *").unwrap(); // every min
-    let mut now = Utc::now();
+    // let thursday_schedule = Schedule::from_str("0 */2 * * * *").unwrap(); // every 2 mins
+    let thursday_schedule = Schedule::from_str("* * * * * 5").unwrap();
+    let friday_schedule = Schedule::from_str("* * * * * 6").unwrap();
+    // let friday_schedule = Schedule::from_str("0 */3 * * * *").unwrap();
 
-    loop {
-        // Compute the time until the next event.
-        if let Some(next) = schedule.upcoming(chrono::Utc).next() {
-            if next > now {
-                let duration = (next - now).to_std().unwrap();
-                info!("Waiting until next scheduled event [{}]", next);
-                tokio::time::sleep(duration).await;
+    let database_clone = database.clone();
+
+    // Spawn a new task for the Thursday schedule
+    tokio::spawn(async move {
+        let mut now = Utc::now();
+        loop {
+            // Compute the time until the next Thursday event.
+            if let Some(next_thursday) = thursday_schedule.upcoming(chrono::Utc).next() {
+                if next_thursday > now {
+                    let duration = (next_thursday - now).to_std().unwrap();
+                    info!(
+                        "[Sending Tickets] Waiting until next scheduled event: [{}]",
+                        next_thursday
+                    );
+                    tokio::time::sleep(duration).await;
+                    // Run task here.
+                    match database_clone.update_all_submitted_to_processing().await {
+                        Ok(_) => info!("Changed successfully to processing records"),
+                        Err(e) => error!("Failed to updated processing records: {}", e),
+                    }
+                    // Update "now" for the next iteration.
+                    now = Utc::now();
+                }
             }
         }
+    });
 
-        // Run your task here.
-        match database.update_all_submitted_to_processing().await {
-            Ok(_) => info!("Records updated successfully"),
-            Err(e) => error!("Failed to updated records: {}", e),
+    // Spawn a new task for the Friday schedule
+    tokio::spawn(async move {
+        let mut now = Utc::now();
+        loop {
+            if let Some(next_friday) = friday_schedule.upcoming(chrono::Utc).next() {
+                if next_friday > now {
+                    let duration = (next_friday - now).to_std().unwrap();
+                    info!(
+                        "[Sent Tickets] Waiting until next scheduled event: [{}]",
+                        next_friday
+                    );
+                    tokio::time::sleep(duration).await;
+                    match database.update_all_processing_to_completed().await {
+                        Ok(_) => info!("Changed successfully to completed records"),
+                        Err(e) => error!("Failed to updated completed records: {}", e),
+                    }
+                    now = Utc::now();
+                }
+            }
         }
-
-        // Update "now" for the next iteration.
-        now = Utc::now();
-    }
+    });
 }

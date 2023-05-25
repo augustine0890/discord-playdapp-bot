f4f616becd55be74ff76a1e4bd3fb0d164c2745e
augustine <augustino0890@gmail.com>
2023-05-25 19:34:35 +0900

    lib: organize the modules, package, and how to import the modules

--- src/database/mongo.rs
+++ src/database/mongo.rs
@@ -1,52 +1,14 @@
 use bson::Bson;
-use chrono::Utc;
 use futures::stream::StreamExt;
-use mongodb::bson::{doc, oid::ObjectId};
+use mongodb::bson::doc;
 use mongodb::error::Error;
 use mongodb::error::Result as MongoResult;
 use mongodb::{
     options::{ClientOptions, FindOneOptions, FindOptions},
     Client, Database,
 };
-use serde::{Deserialize, Serialize};
-use std::fmt;
 
-#[derive(Serialize, Deserialize, Clone, Copy, Debug, Default)]
-pub enum ExchangeStatus {
-    #[default]
-    Submitted,
-    Processing,
-    Completed,
-}
-
-impl fmt::Display for ExchangeStatus {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            ExchangeStatus::Submitted => write!(f, "Submitted"),
-            ExchangeStatus::Processing => write!(f, "Processing"),
-            ExchangeStatus::Completed => write!(f, "Completed"),
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, Default)]
-pub struct Exchange {
-    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    pub id: Option<ObjectId>,
-    #[serde(skip_deserializing)]
-    pub dc_id: String,
-    pub dc_username: String,
-    pub wallet_address: Option<String>,
-    pub item: String,
-    pub quantity: i64,
-    pub status: ExchangeStatus,
-    #[serde(rename = "createdAt", skip_deserializing)]
-    #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
-    pub created_at: chrono::DateTime<Utc>,
-    #[serde(rename = "updatedAt")]
-    #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
-    pub updated_at: chrono::DateTime<Utc>,
-}
+use super::models::{Exchange, ExchangeStatus};
 
 #[derive(Clone)]
 pub struct MongoDB {
